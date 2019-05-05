f2e9c637246ff118e8989cc723d0880a879ce97e
Kostadin Bajalcaliev <kbajalc@gmail.com>
2019-05-05 19:57:54 +0200

    micros and lapse

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,8 +1,8 @@
 const LOAD = process.hrtime();
 const ZERO = Date.now();
 
-export function micros(): number {
-  const hd = process.hrtime(LOAD);
+export function micros(start?: [number, number]): number {
+  const hd = process.hrtime(start || LOAD);
   const now = (ZERO + hd[0] * 1000) * 1000 + Math.floor(hd[1] / 1000);
   return now;
 }
@@ -12,7 +12,10 @@ export function time(): [number, number] {
 }
 
 export function span(start: [number, number], offset?: number): string {
-  const lapse = process.hrtime(start);
+  return lapse(process.hrtime(start));
+}
+
+export function lapse(lapse: [number, number], offset?: number) {
   const ms = (lapse[0] * 1000) + Math.floor(lapse[1] / 1000000) - (offset || 0);
   const ns = Math.floor((lapse[1] % 1000000) / 1000) / 1000;
   return `${ms}${ns.toFixed(3).substring(1)}`;
